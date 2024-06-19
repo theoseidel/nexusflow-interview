03a8f6b05ee39ec06828ad9b5708513f122cb535
Theo Seidel <seidelt7@gmail.com>
2024-06-18 17:45:12 -0700

    add readme

--- repoviewer/src/components/LoadingSpinner.js
+++ repoviewer/src/components/LoadingSpinner.js
@@ -4,7 +4,7 @@ import styles from '../styles.module.css';
 
 const LoadingSpinner = () => (
     <div className={styles.loading}>
-        <GitHubIcon className={styles.spinner} />
+        <GitHubIcon className={styles.spinner} width="60" height="60" color="#007bff" /> {/* Increase the size of the icon */}
     </div>
 );
 
